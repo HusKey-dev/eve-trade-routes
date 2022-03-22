000d377063f101e95ba038a77731f1015801b3af
HusKey-dev <mypost31@mail.ru>
2022-03-22 14:47:08 +0300

    search window now closes after error

--- src/reducers/searchModalReducer.js
+++ src/reducers/searchModalReducer.js
@@ -2,6 +2,8 @@ export default (state = false, action) => {
     switch (action.type) {
         case "TOGGLE_SEARCHMODAL":
             return !state;
+        case "CLOSE_ERRORMESSAGE":
+            return false;
         default:
             return state;
     }
